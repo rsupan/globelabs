ea3c44af1711a7020f7c09606b5de2b5e3618b17
Rey Supan <reysupan7@gmail.com>
2015-08-04 16:56:23 +0800

    Initial commit

---
+++ src/Globelabs/SMS.php
@@ -0,0 +1,11 @@
+<?php
+namespace Globelabs;
+
+/**
+ * SMS
+ */
+class SMS {
+	public static function call() {
+		return 'Hello world';
+	}
+}
---
+++ tests/tests.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Globelabs\SMS;
+
+echo SMS::call();
+?>
